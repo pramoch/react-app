859a1aec075b5149e960deb3fb6ee1a06d6111f3
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-06-13 19:53:45 +0700

    Implement inline error

--- src/components/OTP/OTP.js
+++ src/components/OTP/OTP.js
@@ -14,6 +14,9 @@ const OTP = () => {
   const { t } = useTranslation();
   const classes = useStyles();
   const [mobileNumber, setMobileNumber] = useState('');
+  const [mobileNumberTouched, setMobileNumberTouched] = useState(false);
+  const [otp, setOtp] = useState('');
+  const [otpTouched, setOtpTouched] = useState(false);
 
   const onKeyDown = (event) => {
     const isAllowedKey = event.ctrlKey || event.altKey
@@ -28,12 +31,28 @@ const OTP = () => {
     }
   };
 
+  const onRequestOtp = () => {
+    console.log('Mobile Number = ' + mobileNumber);
+  }
+
+  const onConfirm = () => {
+    console.log('OTP = ' + otp);
+  }
+
+  const isMobileNumberError = () => {
+    return mobileNumberTouched && mobileNumber.length < 10;
+  }
+
+  const isOtpError = () => {
+    return otpTouched && otp.length < 6;
+  }
+
   return (
     <Box fontSize={12}>
       <form>
         <div className={styles.inputWrapper}>
           <div>{ t('ENTER_MOBILE_NUMBER') }</div>
-          <Box display="flex" alignItems="center">
+          <Box display="flex" alignItems="flex-start">
             <Box flex="1">
               <TextField
                 InputProps={{
@@ -52,10 +71,19 @@ const OTP = () => {
                 onKeyDown={onKeyDown}
                 onInput={(e) => setMobileNumber(e.target.value)}
                 defaultValue={mobileNumber}
+                error={isMobileNumberError()}
+                helperText={isMobileNumberError() ? t('MOBILE_NUMBER_ERROR') : ''}
+                onBlur={() => {setMobileNumberTouched(true)}}
               />
             </Box>
             <Box className={styles.otpButtonWrapper}>
-              <Button size="small" variant="contained" color="primary" disabled={mobileNumber.length < 10}>{ t('REQUEST_OTP') }</Button>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                disabled={mobileNumber.length < 10}
+                onClick={onRequestOtp}
+              >{ t('REQUEST_OTP') }</Button>
             </Box>
           </Box>
         </div>
@@ -67,15 +95,30 @@ const OTP = () => {
                 input: classes.input
               }
             }}
+            inputProps={{
+              maxLength: 6
+            }}
             variant="outlined"
             size="small"
             margin="dense"
             placeholder={ t('OTP') }
             fullWidth
+            onKeyDown={onKeyDown}
+            onInput={(e) => setOtp(e.target.value)}
+            defaultValue={otp}
+            error={isOtpError()}
+            helperText={isOtpError() ? t('OTP_ERROR') : ''}
+            onBlur={() => { setOtpTouched(true) }}
           />
         </div>
         <Box textAlign="center">
-          <Button size="small" variant="contained" color="primary">{ t('CONFIRM') }</Button>
+          <Button
+            size="small"
+            variant="contained"
+            color="primary"
+            disabled={otp.length < 6}
+            onClick={onConfirm}
+          >{ t('CONFIRM') }</Button>
         </Box>
       </form>
     </Box>
